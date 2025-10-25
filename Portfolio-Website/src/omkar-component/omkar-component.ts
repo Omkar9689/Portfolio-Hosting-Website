cb2bfa2a465d9f48b0e5c8cc418857bf541b5033
omkar9689 <omkarbarbade12800@gmail.com>
2025-10-25 11:59:49 +0530

    Unable to Load Files Issue

---
+++ Portfolio-Website/src/omkar-component/omkar-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-omkar-component',
+  imports: [],
+  templateUrl: './omkar-component.html',
+  styleUrl: './omkar-component.css',
+})
+export class OmkarComponent {
+
+}
